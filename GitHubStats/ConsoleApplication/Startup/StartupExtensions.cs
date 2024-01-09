a5c826f65a208f455466117f6c41974bb87676f7
Matthew Lewis <mlewis@singular.co.za>
2024-01-09 13:29:48 +0200

    Updated appsettings file logic and added a custom 'Action' configuration mode

--- GitHubStats/ConsoleApplication/Startup/StartupExtensions.cs
+++ GitHubStats/ConsoleApplication/Startup/StartupExtensions.cs
@@ -4,12 +4,28 @@ using Microsoft.Extensions.Configuration;
 
 public static class StartupExtensions
 {
+  public static string GetApplicationPath(string? file = null, string? folders = null)
+  {
+    // Base directory path
+    var currentDirectory = Directory.GetCurrentDirectory();
+
+    // Include project folder if running via GitHub actions (not locally)
+#if ACTION
+    currentDirectory += "\\ConsoleApplication";
+#endif
+
+    // Include path/folders
+    var directory = currentDirectory;
+    directory = string.IsNullOrEmpty(folders) == false ? Path.Combine(directory, folders) : directory;
+    directory = string.IsNullOrEmpty(file) == false ? Path.Combine(directory, file) : directory;
+
+    return directory;
+  }
+
   public static IConfigurationBuilder AddApplicationConsoleConfigurations(this IConfigurationBuilder configBuilder)
   {
     // Configure the appsettings.json
-    //var currentDirectory = Directory.GetCurrentDirectory();
-    var currentDirectory = AppContext.BaseDirectory;
-    var appSettingsPath = Path.Combine(currentDirectory, "Startup\\appsettings.json");
+    var appSettingsPath = GetApplicationPath(file: "appsettings.json", folders: "Startup");
     configBuilder.AddJsonFile(appSettingsPath, optional: false, reloadOnChange: true);
 
     return configBuilder;
