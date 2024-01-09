03695ece32df2f7f47dbc3dc5d6e9927f504aa62
Matthew Lewis <mlewis@singular.co.za>
2024-01-09 10:32:10 +0200

    Added appsettings, startup extensions and a temporary github action for testing

--- GitHubStats/ConsoleApplication/Startup/Program.cs
+++ GitHubStats/ConsoleApplication/Startup/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+using ConsoleApplication.Startup;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+// Configuration application console host
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config => config.AddApplicationConsoleConfigurations())
+    .Build();
+
+// Testing logic
+var configuration = host.Services.GetRequiredService<IConfiguration>();
+var testSecret = configuration["SecretTest"];
+Console.WriteLine("Testing appsettings, GitHub secrets and user secrets setup using placeholder values that can be exposed\n");
+Console.WriteLine($"Secret value = {testSecret}");
+
+// Keep console open by prompting input
+Console.ReadLine();
---
+++ GitHubStats/ConsoleApplication/Startup/StartupExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApplication.Startup;
+
+using Microsoft.Extensions.Configuration;
+
+public static class StartupExtensions
+{
+  public static IConfigurationBuilder AddApplicationConsoleConfigurations(this IConfigurationBuilder configBuilder)
+  {
+    configBuilder.AddJsonFile("Startup\\appsettings.json", optional: false, reloadOnChange: true);
+
+    return configBuilder;
+  }
+}
