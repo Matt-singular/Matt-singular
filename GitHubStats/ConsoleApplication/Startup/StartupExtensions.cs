735af066f748dbab9f77498e0e4b783ccf5d695d
Matthew Lewis <mlewis@singular.co.za>
2024-01-09 15:53:41 +0200

    Cleanup of config startup logic

--- GitHubStats/ConsoleApplication/Startup/StartupExtensions.cs
+++ GitHubStats/ConsoleApplication/Startup/StartupExtensions.cs
@@ -4,19 +4,22 @@ using Microsoft.Extensions.Configuration;
 
 public static class StartupExtensions
 {
-  public static string GetApplicationPath(string? file = null, string? folders = null)
+  public static string GetApplicationPath(string? file = null, string? folder = null)
   {
     // Base directory path
     var applicationPath = Directory.GetCurrentDirectory();
 
+    // Set the path separator
+    static string setPathSeparator(string path) => $"/{path}";
+
     // Include project folder if running via GitHub actions (not locally)
 #if ACTION
-  applicationPath += "/ConsoleApplication";
+  applicationPath += setPathSeparator("ConsoleApplication");
 #endif
 
     // Include path/folders
-    applicationPath += string.IsNullOrEmpty(folders) == false ? $"/{folders}" : string.Empty;
-    applicationPath += string.IsNullOrEmpty(file) == false ? $"/{file}" : string.Empty;
+    applicationPath += string.IsNullOrEmpty(folder) == false ? setPathSeparator(folder) : string.Empty;
+    applicationPath += string.IsNullOrEmpty(file) == false ? setPathSeparator(file) : string.Empty;
 
     return applicationPath;
   }
@@ -24,7 +27,7 @@ public static class StartupExtensions
   public static IConfigurationBuilder AddApplicationConsoleConfigurations(this IConfigurationBuilder configBuilder)
   {
     // Configure the appsettings.json
-    var appSettingsPath = GetApplicationPath(file: "appsettings.json", folders: "Startup");
+    var appSettingsPath = GetApplicationPath(file: "appsettings.json", folder: "Startup");
     configBuilder.AddJsonFile(appSettingsPath, optional: false, reloadOnChange: true);
 
     return configBuilder;
