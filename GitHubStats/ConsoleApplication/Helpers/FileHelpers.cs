f479d96c94afa4624685a51cf56e45f653100989
Matthew Lewis <mlewis@singular.co.za>
2024-01-09 16:48:48 +0200

    Cleanup of startup logic

---
+++ GitHubStats/ConsoleApplication/Helpers/FileHelpers.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApplication.Helpers;
+
+public static class FileHelpers
+{
+  /// <summary>
+  /// A file helper to get the application path including the optional file and folders provided
+  /// (this function will provide the correct application path regardless of the build configuration used and whether it was run in GitHub action or locally)
+  /// </summary>
+  /// <param name="file">Optional file name parameter</param>
+  /// <param name="folders">Optional folder names</param>
+  /// <returns>The application's path</returns>
+  public static string GetApplicationPath(string? file = null, params string[] folders)
+  {
+    // Base directory path
+    var applicationPath = Directory.GetCurrentDirectory();
+
+    // Include project folder if running via GitHub actions (not locally)
+#if ACTION
+    // This logic is required when run via a GitHub action as the current directory will be "GitHubStats" not "ConsoleApplication"
+    applicationPath = string.Join(Constants.PathSeparator, Constants.ConsoleApplicationProject);
+#endif
+
+    // Include path/folders if provided
+    applicationPath = folders.Length > 0 ? string.Join(Constants.PathSeparator, new string[] { applicationPath }.Concat(folders)) : applicationPath;
+    applicationPath = string.IsNullOrEmpty(file) == false ? string.Join(Constants.PathSeparator, applicationPath, file) : string.Empty;
+
+    return applicationPath;
+  }
+  
+
+  /// <summary>
+  /// Localised FileHelper constants
+  /// </summary>
+  public static class Constants
+  {
+    public const char PathSeparator = '/';
+    public const string ConsoleApplicationProject = "ConsoleApplication";
+  }
+}
--- GitHubStats/ConsoleApplication/Startup/StartupExtensions.cs
+++ GitHubStats/ConsoleApplication/Startup/StartupExtensions.cs
@@ -1,35 +1,30 @@
 ﻿namespace ConsoleApplication.Startup;
 
+using ConsoleApplication.Helpers;
 using Microsoft.Extensions.Configuration;
 
 public static class StartupExtensions
 {
-  public static string GetApplicationPath(string? file = null, string? folder = null)
-  {
-    // Base directory path
-    var applicationPath = Directory.GetCurrentDirectory();
-
-    // Set the path separator
-    static string setPathSeparator(string path) => $"/{path}";
-
-    // Include project folder if running via GitHub actions (not locally)
-#if ACTION
-  applicationPath += setPathSeparator("ConsoleApplication");
-#endif
-
-    // Include path/folders
-    applicationPath += string.IsNullOrEmpty(folder) == false ? setPathSeparator(folder) : string.Empty;
-    applicationPath += string.IsNullOrEmpty(file) == false ? setPathSeparator(file) : string.Empty;
-
-    return applicationPath;
-  }
-
+  /// <summary>
+  /// Sets up the application's config files at Startup
+  /// </summary>
+  /// <param name="configBuilder"></param>
+  /// <returns>the configured IConfigurationBuilder</returns>
   public static IConfigurationBuilder AddApplicationConsoleConfigurations(this IConfigurationBuilder configBuilder)
   {
     // Configure the appsettings.json
-    var appSettingsPath = GetApplicationPath(file: "appsettings.json", folder: "Startup");
+    var appSettingsPath = FileHelpers.GetApplicationPath(file: Constants.AppSettingsFile, folders: Constants.AppSettingsFolder);
     configBuilder.AddJsonFile(appSettingsPath, optional: false, reloadOnChange: true);
 
     return configBuilder;
+  }
+
+  /// <summary>
+  /// Localised StartupExtension constants
+  /// </summary>
+  public static class Constants
+  {
+    public const string AppSettingsFile = "appsettings.json";
+    public const string AppSettingsFolder = "Startup";
   }
 }
