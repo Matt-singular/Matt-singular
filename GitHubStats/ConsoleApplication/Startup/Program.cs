39dfacd94fe073c4378764edca117e0bdf1ce1e5
Matthew Lewis <mlewis@singular.co.za>
2024-01-13 11:19:43 +0200

    Updated the application entry point logic

--- GitHubStats/ConsoleApplication/Startup/Program.cs
+++ GitHubStats/ConsoleApplication/Startup/Program.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.Extensions.Hosting;
 using ConsoleApplication.Startup;
+using Services.Startupp;
+using Microsoft.Extensions.DependencyInjection;
 
 // Configuration application console host
 var host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration(config => config.AddApplicationConsoleConfigurations())
-    .Build();
+  .ConfigureAppConfiguration(config => config.AddApplicationConsoleConfigurations())
+  .ConfigureServices(services => services.AddApplicationServices())
+  .Build();
 
+// Temporary for debugging purposese
+var testService = host.Services.GetRequiredService<Services.IGitHubRestService>();
+await testService.GetGitHubUserStatistics().ConfigureAwait(false);
+//await testService.GetGitHubDiffForRange().ConfigureAwait(false);
+//var testService = host.Services.GetRequiredService<Services.IGitHubGraphQlService>();
+//await testService.GetCommitStatisticsForUser().ConfigureAwait(false);
 
 // Keep console open by prompting input
+Console.Write("Application built successfully... ");
 Console.ReadLine();
