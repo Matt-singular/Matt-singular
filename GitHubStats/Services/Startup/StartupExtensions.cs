b497575e99ef026c71317c7e1523a8d9ad18f228
Matthew Lewis <mlewis@singular.co.za>
2024-01-13 11:17:45 +0200

    Added StartupExtensions for the Services project

---
+++ GitHubStats/Services/Startup/StartupExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Services.Startupp;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+public static class StartupExtensions
+{
+  /// <summary>
+  /// Dependency Injection logic for the application's services
+  /// </summary>
+  /// <param name="serviceCollection"></param>
+  /// <returns>The configured application's service collection</returns>
+  public static IServiceCollection AddApplicationServices(this IServiceCollection serviceCollection)
+  {
+    // Configure the DI container for Services project
+    serviceCollection.TryAddScoped<IGitHubRestService, GitHubRestService>();
+    serviceCollection.TryAddScoped<IGitHubGraphQlService, GitHubGraphQlService>();
+
+    return serviceCollection;
+  }
+}
